5be3f61287e9455da9dba0f4add183d815718909
cillami <cillaminami@gmail.com>
2018-01-18 15:07:46 +0100

    drinks for ya

--- __tests__/pizza.js
+++ __tests__/pizza.js
@@ -9,3 +9,22 @@ describe('pizza', () => {
 
 })
 
+describe('drinks', () => {
+
+  test('if no people, return 0', () => {
+    expect(pizza.calculate1lDrink(0)).toBe(false);
+  })
+
+  test('if five people, return true', () => {
+    const result = pizza.calculate1lDrink(5);
+    expect(result).toBe(true);
+  })
+
+  test('if five people, return 1', () => {
+    const result = pizza.calculate2lDrink(5);
+    expect(result).toBe(1);
+  })
+
+})
+
+
--- src/pizza.js
+++ src/pizza.js
@@ -1,7 +1,28 @@
 module.exports = {
+  calculatePizza(people) {
+    return 0;
+  },
 
-	calculatePizza(people){
-		return 0;
-	}
-	
-}
+  calculate1lDrink(people) {
+    const liter = people * 0.5; // 2.5
+
+    const roundUp = Math.ceil(liter); // 3
+    const resultOfMod = roundUp % 2; // 1
+
+    let drink1l = false;
+
+    if (resultOfMod === 1) {
+      drink1l = true;
+    } else {
+      drink1l = false;
+    }
+
+    return drink1l;
+  },
+
+  calculate2lDrink(people) {
+    const liter = people * 0.5;
+    const drinks = Math.floor(liter / 2);
+    return drinks;
+  }
+};
